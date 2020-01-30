5e5a0c2633061a35025b6960b16da0308905ce11
Aftery <aliezknow@gmail.com>
2020-01-30 19:07:17 +0800

    :pencil2:代码整理

--- tensquare_article/src/main/java/top/aftery/article/pojo/Column.java
+++ tensquare_article/src/main/java/top/aftery/article/pojo/Column.java
@@ -23,13 +23,20 @@ import java.io.Serializable;
 public class Column implements Serializable{
 
 	@Id
-	private String id;//ID
-	private String name;//专栏名称
-	private String summary;//专栏简介
-	private String userid;//用户ID
-	private java.util.Date createtime;//申请日期
-	private java.util.Date checktime;//审核日期
-	private String state;//状态
+	//ID
+	private String id;
+	//专栏名称
+	private String name;
+	//专栏简介
+	private String summary;
+	//用户ID
+	private String userid;
+	//申请日期
+	private java.util.Date createtime;
+	//审核日期
+	private java.util.Date checktime;
+	//状态
+	private String state;
 
 
 }
